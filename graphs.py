c4bad9956166e6a14e748a2ad26ba82d264d07bb
R11G <richlgame31@gmail.com>
2023-11-29 14:48:58 -0500

    some distance vector implementation

--- graphs.py
+++ graphs.py
@@ -1,6 +1,34 @@
 def init():
     return 0
 def distance_vector():
-    return 0
+    tables = dict()
+    for x in node_dict:
+        tables[x] = []
+        for i in range(len(node_dict)):
+            row = []
+            for j in range(len(node_dict)):
+                row.append(-1)
+            tables[x].append(row)
+        for end, weight in node_dict[x]:
+            tables[x][x][end] = weight
+    for x in tables:
+        prop = tables[x]
+        for y, weight in node_dict[x]:
+            xydist = prop[x][y]
+            tables[y][x] = prop[x] # need to copy all non-self rows
+            yval = tables[y][y]
+            tables[y] = prop
+            propval = prop[x]
+            for z in propval:
+                z += xydist
+            for i,z in enumerate(propval):
+                if yval[i] < z:
+                    tables[y][y][i] = yval[i]
+                else:
+                    tables[y][y][i] = z
+    return tables
+def remove_node_dv(node, tables):
+    temp = tables[node]
+    del tables[node]
 def link_state():
     return 0
