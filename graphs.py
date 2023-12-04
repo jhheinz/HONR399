706722ba3d14e110dca0c0c625b2f0597fd74049
R11G <richlgame31@gmail.com>
2023-12-04 16:51:22 -0500

    Fixed distance vector functions

--- graphs.py
+++ graphs.py
@@ -1,49 +1,56 @@
 def init():
     return 0
-def init_dv():
+def init_dv(node_dict):
     tables = dict()
     for x in node_dict:
         tables[x] = dict()
         for y in node_dict:
-            tables[x][y] = dict()
-            for z in node_dict:
-                tables[x][y][z] = float('inf')
-        for end, weight in node_dict[x]:
-            tables[x][x][end] = weight
-def distance_vector(tables):
-    for x in tables:
-        prop = tables[x]
-        for y, weight in node_dict[x]:
-            xydist = prop[x][y]
-            yval = tables[y][y]
-            tables[y] = prop
-            propval = prop[x]
-            for z in propval:
-                z += xydist
-            for z in prop:
-                if yval[z] < propval[z]:
-                    tables[y][y][z] = yval[z]
-                else:
-                    tables[y][y][z] = propval[z]
+            tables[x][y] = (float('inf'),-1)
+        for y in node_dict[x]:
+            tables[x][x] = (node_dict[x][y][1], y)
+    return tables
+def distance_vector(node_dict, tables):
+    count = 0
+    changesC = 0
+    for i in tables:
+        for x in tables:
+            prop = tables[x]
+            for y in node_dict[x]:
+                xydist = prop[y][0]
+                yval = tables[y]
+                for z in prop:
+                    if yval[z] > prop[z][0]+xydist:
+                        tables[y][z] = (prop[z][0]+xydist, prop[z][1])
+                        changesC+=1
+                count+=1
+    print("Update counter:", count)
+    print("Changed value counter:", changesC)
     return tables
 def remove_node_dv(node, tables):
     for x in tables:
         del tables[x][node]
-        for y in tables[x]:
-            del y[node]
     del tables[node]
-def add_node_dv(node, tables):
+    return tables
+def add_node_dv(node_dict, node, tables):
     for x in tables:
-        for y in tables[x]:
-            tables[x][y][node] = float('inf')
-            tables[x][node][y] = float('inf')
+        tables[x][node] = (float('inf'),-1)
     tables[node] = dict()
     for x in node_dict:
-        tables[node][x] = dict()
-        for y in node_dict:
-            tables[node][x][y] = float('inf')
-    for end,weight in node_dict[node]:
-        tables[node][node][end] = weight
-        tables[end][end][node] = weight
+        tables[node][x] = (float('inf'),-1)
+    for x in node_dict[node]:
+        weight = node_dict[node][x][1]
+        tables[node][x] = (weight, x)
+        tables[x][node] = (weight, node)
+    return tables
+def remove_link_dv(start, end, tables):
+    tables[start][end] = (float('inf'),-1)
+    tables[end][start] = (float('inf'),-1)
+    return tables
+def add_link_dv(start, end, tables, node_dict):
+    if tables[start][end][0] > node_dict[start][end][1]:
+        tables[start][end] = (node_dict[start][end][1], end)
+    if tables[end][start][0] > node_dict[end][start][1]:
+        tables[end][start] = (node_dict[end][start][1], start)
+    return tables
 def link_state():
     return 0
